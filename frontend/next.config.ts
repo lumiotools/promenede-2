d4624e553408cb0b7e7e9f0bd10a5f32ab30dc97
subho004 <haiisubhajit2003@gmail.com>
2025-03-21 22:57:03 +0530

    fix: build fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enables React Strict Mode
+  swcMinify: true, // Use SWC for faster minification
 };
 
 export default nextConfig;
