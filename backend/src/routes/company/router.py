336b27c3740d6c5c97dd447c64308bed48901e51
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-03-24 14:43:52 +0530

    Added Backend for Employeed Reviews SUmmary

--- backend/src/routes/company/router.py
+++ backend/src/routes/company/router.py
@@ -10,6 +10,8 @@ from src.utils.secFilings.analyse10K import analyze_10K_filing
 from src.utils.coresignal.company import get_company_details,get_company_id
 from src.utils.yahoo.shareholder import get_shareholder_info
 from src.routes.company.helpers import extract_financial_data, calculate_per, calculate_revenue_growth, get_employee_review_trend, get_acquisitions, extract_company_timeline, extract_product_details, extract_product_timeline, extract_strategic_development, extract_company_strategy, extract_customer_success, extract_value_chain, extract_market_map, extract_competitive_landscape, extract_financial_comparables, combine_funding_and_founding, combine_webtraffic_and_founding, extract_company_name_from_website, extract_regulation_info, extract_opportunities, extract_risks, extract_common_questions, generate_competitors_answer, generate_technologies_answer,convert_null_to_none
+
+from src.utils.llm_summary.employeeReviews import get_employee_reviews_summary
 # Load environment variables from .env file
 load_dotenv()
 
@@ -50,6 +52,15 @@ async def get_company_data(request: CompanyRequest):
     coresignal_data = get_company_details(company_enrichment_id)
     coresignal_data=convert_null_to_none(coresignal_data)
     llmData =  fetch_company_data(company_name, 10000, "gpt-4o-mini")
+    
+    key_members = []
+    
+    for i, member in enumerate(llmData["leadership_executives"]):
+        key_members.append({
+            "member_id": i,
+            "member_full_name": member["name"],
+            "member_position_title": member["position"]
+        })
 
     
     # If critical data is missing, then try SEC as fallback
@@ -170,7 +181,7 @@ async def get_company_data(request: CompanyRequest):
                 "breakdown_by_region": coresignal_data.get("employees_count_breakdown_by_region", {}),
                 "breakdown_by_seniority": coresignal_data.get("employees_count_breakdown_by_seniority", {})
             },
-            "key_members": coresignal_data.get("key_executives", []),
+            "key_members": key_members,
             "leadership_executives":llmData["leadership_executives"],
             "leadership": {
                 "key_executives": coresignal_data.get("key_executives", []),
@@ -266,6 +277,13 @@ async def get_company_data(request: CompanyRequest):
             "discord_url": coresignal_data.get("discord_url", [])
         }
     }
+    
+    
+    employeeReviewsSummary = get_employee_reviews_summary(response_data["organization"]["employee_reviews2"])
+    
+    response_data["organization"]["employee_reviews2"]["summary"] = employeeReviewsSummary
+    
+    print(employeeReviewsSummary)
     
     return {"success":True,"company_name": company_name, "data": response_data}
 
